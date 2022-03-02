56e20e2c964e7505b969dbb6303f9232e8c06fd6
Ruben <ruben.infantes@gmail.com>
2022-03-02 16:54:18 +0100

    iteraction#1 complete

--- iteracion_01.js
+++ iteracion_01.js
@@ -4,3 +4,14 @@ const movies = [
     { title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller'] },
     { title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación'] },
 ]
+const moviesCatergory = []
+
+
+for (let movie of movies) {
+    for (let category of movie.categories) {
+        if (moviesCatergory.indexOf(category) === -1) {
+            moviesCatergory.push(category)
+        }
+    }
+
+} console.log(moviesCatergory)
