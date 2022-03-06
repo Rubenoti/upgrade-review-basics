dacc33e00e7930af43ae58a1848072d4c76f9f63
Ruben <ruben.infantes@gmail.com>
2022-03-06 13:58:49 +0100

    iteraction#4 complete

--- iteracion_04.js
+++ iteracion_04.js
@@ -1,3 +1,14 @@
 const bugs = ['Caracol', 'Mosquito', 'Salamandra', 'Ajolote']
 
-function findArrayIndex(array, text) { }
+function findArrayIndex(array, text) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === text) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+
+console.log(findArrayIndex(bugs, 'Mosquito'))
